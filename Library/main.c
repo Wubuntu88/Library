79a037071279b3ca07811cdd41c8854d58787bf1
William Edward Gillespie <Will@williams-macbook-air.local>
2016-02-25 16:24:18 -0500

    Initial Commit

---
+++ Library/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Library
+//
+//  Created by William Edward Gillespie on 2/25/16.
+//  Copyright © 2016 William Edward Gillespie. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
