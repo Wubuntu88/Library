ce5c9d092842618bb9525b8cb7a1c2c09a8f81cf
William Edward Gillespie <wgilles1@emich.edu>
2016-03-06 15:49:05 -0500

    Added the isValidISBN method.

--- Library/main.c
+++ Library/main.c
@@ -17,13 +17,23 @@
 int doesContainUserIdAndPassword(int inputUserId, int inputPassword);
 void getBookInformationFromFile(BookInfo bookInfo[], int *size);
 int writeBookInformationToFile(BookInfo bookInfo[], int size);
+int isValidISBN(char isbn[], int size);
 
 int main(int argc, const char * argv[]) {
     BookInfo bookInfo[10];
     int nElems;// = &q;
     memset(bookInfo, 0, sizeof(bookInfo));
     getBookInformationFromFile(bookInfo, &nElems);
-    writeBookInformationToFile(bookInfo, nElems);
+    for (int i = 0; i < nElems; i++) {
+        int valid = isValidISBN(bookInfo[i].isbn, sizeof(bookInfo[i].isbn));
+        if (valid) {
+            printf("valid\n");
+        }else{
+            printf("invalid\n");
+        }
+    }
+
+    //writeBookInformationToFile(bookInfo, nElems);
     //printf("S: %lu\n", sizeof(bookInfo)/ sizeof(BookInfo));
     /*
     int i = 0;
@@ -194,8 +204,47 @@ int doesContainUserIdAndPassword(int inputUserId, int inputPassword){
     fclose(fp);
     return 0;//did not match
 }
-
-
+/*
+ Returns 1 if valid; 0 if invalid
+ size should be 14
+ */
+int isValidISBN(char isbn[], int size){
+    if (size != 14) {//incorrect size
+        return 0;
+    }
+    int checkBitIndex = size - 2;
+    int checkDigit;
+    if ('0' <= isbn[checkBitIndex] &&  isbn[checkBitIndex] <= '9') {
+        checkDigit = isbn[checkBitIndex] - '0';
+    }else{
+        return 0;//invalid isbn if checkbit isn't numeric
+    }
+    int finalIndex = size - 3;
+    int sumOfWeightedProducts = 0;
+    for (int i = 0; i <= finalIndex; i++) {
+        if ('0' <= isbn[i] &&  isbn[i] <= '9') {
+            if (i % 2 == 0) {
+                sumOfWeightedProducts += (isbn[i] - '0');
+            }else{
+                sumOfWeightedProducts +=(isbn[i] - '0') * 3;
+            }
+        } else {
+            return 0;//invalid isbn if isbn[i] isn't numeric
+        }
+    }
+    
+    int computedCheckDigit = 0;
+    int remainder = sumOfWeightedProducts % 10;
+    if (remainder != 0) {
+        computedCheckDigit = 10 - remainder;
+    }
+    
+    if (computedCheckDigit == checkDigit) {
+        return 1;
+    }else{
+        return 0;
+    }
+}
 
 
 
